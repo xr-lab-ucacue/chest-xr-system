aeee8c90b3305459b6bbb9c7a188e246d5561e1d
Pepe <95196085+PePeWee07@users.noreply.github.com>
2022-10-29 23:40:36 -0500

    new changes login html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,13 @@ import { RadiologyComponent } from './radiology/radiology.component';
 import { RadiologysComponent } from './radiologys/radiologys.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'modelo1', component: Modelo1Component },
   { path: 'radiology', component: RadiologyComponent },
   { path: 'radiologys', component: RadiologysComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: '', pathMatch: 'full', component: Modelo1Component },
-  { path: '**', pathMatch: 'full', component: Modelo1Component }
+  { path: '', pathMatch: 'full', component: LoginComponent },
+  { path: '**', pathMatch: 'full', component: LoginComponent }
 ];
 
 @NgModule({
