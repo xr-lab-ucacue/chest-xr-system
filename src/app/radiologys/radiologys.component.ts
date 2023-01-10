2322fbac1bec24fddf5a23e8ad9996506d121c48
Pepe <95196085+PePeWee07@users.noreply.github.com>
2023-01-10 13:14:24 -0500

    tools cornerstone funcional

--- src/app/radiologys/radiologys.component.ts
+++ src/app/radiologys/radiologys.component.ts
@@ -1,14 +1,11 @@
 import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 
-// import * as cornerstoneMath from "cornerstone-math";
-// import Hammer from "hammerjs";
-// declare const cornerstone;
-// import * as cornerstoneWebImageLoader from "cornerstone-web-image-loader";
-// import cornerstoneWebImageLoader from "cornerstone-web-image-loader";
-import * as cornerstoneTools from '@cornerstonejs/tools';
+import cornerstoneTools from 'cornerstone-tools';
 import * as cornerstone from 'cornerstone-core';
 import cornerstoneWADOImageLoader from "cornerstone-wado-image-loader";
 import * as dicomParser from 'dicom-parser';
+import cornerstoneMath from 'cornerstone-math';
+import  'hammerjs'
 
 
 var config = {
@@ -25,14 +22,15 @@ cornerstoneWADOImageLoader.webWorkerManager.initialize(config);
 import { init as csRenderInit } from '@cornerstonejs/core';
 import { init as csToolsInit } from '@cornerstonejs/tools';
 import { RenderingEngine, Types, Enums, metaData } from '@cornerstonejs/core';
-// import { ViewportType } from '@cornerstonejs/core/dist/esm/enums';
+import { ViewportType } from '@cornerstonejs/core/dist/esm/enums';
 
-import {
-  initDemo,
-  createImageIdsAndCacheMetaData,
-  setTitleAndDescription,
-  ctVoiRange,
-} from 'helpers';
+// import {
+//   initDemo,
+//   createImageIdsAndCacheMetaData,
+//   setTitleAndDescription,
+//   ctVoiRange,
+// } from 'helpers';
+
 
 @Component({
   selector: 'app-radiologys',
@@ -43,21 +41,23 @@ export class RadiologysComponent implements OnInit {
   @ViewChild('testpod') canvasDiv!: ElementRef;
 
 //   async ngOnInit(){
-//     await csRenderInit(), csToolsInit();
+//     // await csRenderInit(), csToolsInit();
 
-//         const {
-//           PanTool,
-//           WindowLevelTool,
-//           StackScrollMouseWheelTool,
-//           ZoomTool,
-//           ToolGroupManager,
-//           Enums: csToolsEnums,
-//         } = cornerstoneTools;
+//     //     const {
+//     //       PanTool,
+//     //       WindowLevelTool,
+//     //       StackScrollMouseWheelTool,
+//     //       ZoomTool,
+//     //       ToolGroupManager,
+//     //       Enums: csToolsEnums,
+//     //     } = cornerstoneTools;
 
-//         const { MouseBindings } = csToolsEnums;
-//         const { ViewportType } = Enums;
+//     //     const { MouseBindings } = csToolsEnums;
+//     //     const { ViewportType } = Enums;
 
-// Get Cornerstone imageIds and fetch metadata into RAM
+
+
+// // Get Cornerstone imageIds and fetch metadata into RAM
 // const imageIds = await createImageIdsAndCacheMetaData({
 //   StudyInstanceUID:
 //     '1.3.6.1.4.1.14519.5.2.1.7009.2403.334240657131972136850343327463',
@@ -95,37 +95,37 @@ export class RadiologysComponent implements OnInit {
 
 // const viewport = <Types.IStackViewport>renderingEngine.getViewport(viewportId);
 
-//  viewport.setStack(imageId);
+//  viewport.setStack(imageIds);
 // viewport.render();
 
-// cornerstoneTools.addTool(ZoomTool);
-// cornerstoneTools.addTool(WindowLevelTool);
+// // cornerstoneTools.addTool(ZoomTool);
+// // cornerstoneTools.addTool(WindowLevelTool);
 
-// const toolGroupId = 'myToolGroup';
-// const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
+// // const toolGroupId = 'myToolGroup';
+// // const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
 
-// // Add tools to the ToolGroup
-// toolGroup.addTool(ZoomTool.toolName);
-// toolGroup.addTool(WindowLevelTool.toolName);
+// // // Add tools to the ToolGroup
+// // toolGroup.addTool(ZoomTool.toolName);
+// // toolGroup.addTool(WindowLevelTool.toolName);
 
-// toolGroup.addViewport(viewportId, renderingEngineId);
+// // toolGroup.addViewport(viewportId, renderingEngineId);
 
-// // Set the windowLevel tool to be active when the mouse left button is pressed
-// toolGroup.setToolActive(WindowLevelTool.toolName, {
-//   bindings: [
-//     {
-//       mouseButton: csToolsEnums.MouseBindings.Primary, // Left Click
-//     },
-//   ],
-// });
+// // // Set the windowLevel tool to be active when the mouse left button is pressed
+// // toolGroup.setToolActive(WindowLevelTool.toolName, {
+// //   bindings: [
+// //     {
+// //       mouseButton: csToolsEnums.MouseBindings.Primary, // Left Click
+// //     },
+// //   ],
+// // });
 
-// toolGroup.setToolActive(ZoomTool.toolName, {
-//   bindings: [
-//     {
-//       mouseButton: csToolsEnums.MouseBindings.Secondary, // Right Click
-//     },
-//   ],
-// });
+// // toolGroup.setToolActive(ZoomTool.toolName, {
+// //   bindings: [
+// //     {
+// //       mouseButton: csToolsEnums.MouseBindings.Secondary, // Right Click
+// //     },
+// //   ],
+// // });
 //   }
 
 
@@ -335,118 +335,229 @@ export class RadiologysComponent implements OnInit {
 
   //--------- Tercera Fomra ----------
 
-  async ngOnInit() {
-    await csRenderInit(), csToolsInit();
+  // async ngOnInit() {
+  //   await csRenderInit(), csToolsInit();
 
-        const {
-          PanTool,
-          WindowLevelTool,
-          StackScrollMouseWheelTool,
-          ZoomTool,
-          ToolGroupManager,
-          Enums: csToolsEnums,
-        } = cornerstoneTools;
+  //       const {
+  //         PanTool,
+  //         WindowLevelTool,
+  //         StackScrollMouseWheelTool,
+  //         ZoomTool,
+  //         ToolGroupManager,
+  //         Enums: csToolsEnums,
+  //       } = cornerstoneTools;
 
-        const { MouseBindings } = csToolsEnums;
-        const { ViewportType } = Enums;
+  //       const { MouseBindings } = csToolsEnums;
+  //       const { ViewportType } = Enums;
+
+  //   cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
+  //   cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
+
+
+
+  //   const content = document.getElementById('content');
+  //   const element = document.createElement('div');
+  //   // Disable the default context menu
+  //   element.oncontextmenu = (e) => e.preventDefault();
+  //   element.style.width = '300px';
+  //   element.style.height = '300px';
+  //   content.appendChild(element);
+  //   await csRenderInit();
+
+
+
+  //   // var element = document.getElementById('element');
+
+  //   // Injects cornerstone "enabled" canvas into DOM
+  //   cornerstone.enable(element);
+
+  //   cornerstone.loadAndCacheImage("wadouri:" + '../../assets/Im221.dcm').then(imageData => {
+  //     console.log(imageData);
+  //     cornerstone.displayImage(element, imageData);
+
+  //       var viewport = {
+  //       invert: false,
+  //       pixelReplication: false,
+  //       voi: {
+  //         windowWidth: 400,
+  //         windowCenter: 200,
+  //         // windowHeight: 900,
+  //       },
+  //       scale: 1.2,
+  //       translation: {
+  //         x: 0,
+  //         y: 0,
+  //       },
+  //       // colormap: 'hot',
+  //     };
+
+  //     cornerstone.setViewport(element, viewport);
+  //     cornerstone.updateImage(element);
+
+  //   }).catch( error => { console.error(error) });
+
+
+  //   const renderingEngineId = 'myRenderingEngine';
+  //   const renderingEngine = new RenderingEngine(renderingEngineId);
+
+  //   const viewportId = 'CT_AXIAL_STACK';
+
+  //   const viewportInput = {
+  //     viewportId,
+  //     element,
+  //     type: ViewportType.STACK,
+  //   };
+
+  //   renderingEngine.enableElement(viewportInput);
+
+  //   //const viewport = <Types.IStackViewport>renderingEngine.getViewport(viewportId);
+
+  //   cornerstoneTools.addTool(ZoomTool);
+  //   cornerstoneTools.addTool(WindowLevelTool);
+
+  //   const toolGroupId = 'myToolGroup';
+  //   const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
+
+  //   // Add tools to the ToolGroup
+  //   toolGroup.addTool(ZoomTool.toolName);
+  //   toolGroup.addTool(WindowLevelTool.toolName);
+
+  //   toolGroup.addViewport(viewportId, renderingEngineId);
+
+  //   // Set the windowLevel tool to be active when the mouse left button is pressed
+  //   toolGroup.setToolActive(WindowLevelTool.toolName, {
+  //     bindings: [
+  //       {
+  //         mouseButton: csToolsEnums.MouseBindings.Primary, // Left Click
+  //       },
+  //     ],
+  //   });
+
+  //   toolGroup.setToolActive(ZoomTool.toolName, {
+  //     bindings: [
+  //       {
+  //         mouseButton: csToolsEnums.MouseBindings.Secondary, // Right Click
+  //       },
+  //     ],
+  //   });
+
+
+  // }
+
+  //>>>>>>>>>>>>>>>>>
+
+
+   ngOnInit() {
+    cornerstoneTools.external.cornerstone = cornerstone;
+    cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
+    cornerstoneTools.external.Hammer = Hammer;
+    cornerstoneTools.init();
+
 
     cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
     cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
 
 
+    var element = document.getElementById('element');
 
-    const content = document.getElementById('content');
-    const element = document.createElement('div');
-    // Disable the default context menu
-    element.oncontextmenu = (e) => e.preventDefault();
-    element.style.width = '300px';
-    element.style.height = '300px';
-    content.appendChild(element);
-    await csRenderInit();
+    const fontFamily =
+  'Work Sans, Roboto, OpenSans, HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif';
+
+    cornerstoneTools.textStyle.setFont(`16px ${fontFamily}`);
+
+    // Set the tool width
+    cornerstoneTools.toolStyle.setToolWidth(2);
+
+    // Set color for inactive tools
+    cornerstoneTools.toolColors.setToolColor('rgb(255, 255, 0)');
+
+    // Set color for active tools
+    cornerstoneTools.toolColors.setActiveColor('rgb(0, 255, 0)');
 
 
+    //>>>..
+    const LengthTool = cornerstoneTools.LengthTool;
+    const  ZoomMouseWheelTool =  cornerstoneTools.ZoomMouseWheelTool;
+    const  EllipticalRoiTool =  cornerstoneTools.EllipticalRoiTool;
+    const  ArrowAnnotateTool =  cornerstoneTools.ArrowAnnotateTool;
+    const  EraserTool =  cornerstoneTools.EraserTool;
+    const  MagnifyTool =  cornerstoneTools.MagnifyTool; // lupa donde este el puntero matenido
+    const  RotateTool =  cornerstoneTools.RotateTool;
+    const WwwcTool = cornerstoneTools.WwwcTool; // brillo
+    const ScaleOverlayTool = cornerstoneTools.ScaleOverlayTool;
+    const PanTool = cornerstoneTools.PanTool;// mover img por el canvas
+    const AngleTool = cornerstoneTools.AngleTool;
+    const BidirectionalTool = cornerstoneTools.BidirectionalTool;
+    const OrientationMarkersTool = cornerstoneTools.OrientationMarkersTool;// como brujula en la img direciones
+    const  FreehandRoiTool =  cornerstoneTools.FreehandRoiTool;// crea lineas a partir de otras (no para hatsa llegar al punto de inico)
+    const RectangleRoiTool = cornerstoneTools.RectangleRoiTool;// rectangulo calcula el area
 
-    // var element = document.getElementById('element');
-
-    // Injects cornerstone "enabled" canvas into DOM
+    // Make sure we have at least one element Enabled
     cornerstone.enable(element);
 
-    cornerstone.loadAndCacheImage("wadouri:" + '../../assets/Im221.dcm').then(imageData => {
+    // Adds tool to ALL currently Enabled elements
+    cornerstoneTools.addTool(LengthTool);
+    cornerstoneTools.addTool(ZoomMouseWheelTool)
+    cornerstoneTools.addTool(EllipticalRoiTool)
+    cornerstoneTools.addTool(ArrowAnnotateTool)
+    cornerstoneTools.addTool(EraserTool)
+    cornerstoneTools.addTool(FreehandRoiTool)
+    cornerstoneTools.addTool(MagnifyTool)
+    cornerstoneTools.addTool(RotateTool)
+    cornerstoneTools.addTool(WwwcTool)
+    cornerstoneTools.addTool(ScaleOverlayTool)
+    cornerstoneTools.addTool(PanTool)
+    cornerstoneTools.addTool(OrientationMarkersTool)
+    cornerstoneTools.addTool(AngleTool)
+    cornerstoneTools.addTool(BidirectionalTool)
+    cornerstoneTools.addTool(RectangleRoiTool)
+
+
+    // Activate the tool for ALL currently Enabled elements
+    cornerstoneTools.setToolActive('Length', { mouseButtonMask: 0 });
+    cornerstoneTools.setToolActive('ZoomMouseWheel',  { mouseButtonMask: 0 })
+    cornerstoneTools.setToolActive('EllipticalRoi',  { mouseButtonMask: 0 })
+    cornerstoneTools.setToolActive('ArrowAnnotate',  { mouseButtonMask: 0 })
+    cornerstoneTools.setToolActive('Eraser',  { mouseButtonMask: 3 })
+    cornerstoneTools.setToolActive('FreehandRoi',  { mouseButtonMask: 0 })
+    cornerstoneTools.setToolActive('Magnify',  { mouseButtonMask: 0 })
+    cornerstoneTools.setToolActive('Rotate',  { mouseButtonMask: 0 })
+    cornerstoneTools.setToolActive('Wwwc',  { mouseButtonMask: 0 })
+    cornerstoneTools.setToolActive('ScaleOverlay', { mouseButtonMask: 0 })
+    cornerstoneTools.setToolActive('Pan', { mouseButtonMask: 0 })
+    cornerstoneTools.setToolActive('OrientationMarkers', { mouseButtonMask: 0 })
+    cornerstoneTools.setToolActive('Angle', { mouseButtonMask: 0 })
+    cornerstoneTools.setToolActive('Bidirectional', { mouseButtonMask: 0 })
+    cornerstoneTools.setToolActive('RectangleRoi', { mouseButtonMask: 1 })
+
+
+
+    cornerstone.loadAndCacheImage("wadouri:" + '../../assets/IM222').then(imageData => {
       console.log(imageData);
       cornerstone.displayImage(element, imageData);
 
-        var viewport = {
-        invert: false,
-        pixelReplication: false,
-        voi: {
-          windowWidth: 400,
-          windowCenter: 200,
-          // windowHeight: 900,
-        },
-        scale: 1.2,
-        translation: {
-          x: 0,
-          y: 0,
-        },
-        // colormap: 'hot',
-      };
+      //   var viewport = {
+      //   invert: false,
+      //   pixelReplication: false,
+      //   voi: {
+      //     windowWidth: 400,
+      //     windowCenter: 200,
+      //     windowHeight: 900,
+      //   },
+      //   scale: 1.2,
+      //   translation: {
+      //     x: 0,
+      //     y: 0,
+      //   },
+      //   // colormap: 'hot',
+      // };
 
-      cornerstone.setViewport(element, viewport);
-      cornerstone.updateImage(element);
+      // cornerstone.setViewport(element, viewport);
+      // cornerstone.updateImage(element);
 
     }).catch( error => { console.error(error) });
 
-
-    const renderingEngineId = 'myRenderingEngine';
-    const renderingEngine = new RenderingEngine(renderingEngineId);
-
-    const viewportId = 'CT_AXIAL_STACK';
-
-    const viewportInput = {
-      viewportId,
-      element,
-      type: ViewportType.STACK,
-    };
-
-    renderingEngine.enableElement(viewportInput);
-
-    //const viewport = <Types.IStackViewport>renderingEngine.getViewport(viewportId);
-
-    cornerstoneTools.addTool(ZoomTool);
-    cornerstoneTools.addTool(WindowLevelTool);
-
-    const toolGroupId = 'myToolGroup';
-    const toolGroup = ToolGroupManager.createToolGroup(toolGroupId);
-
-    // Add tools to the ToolGroup
-    toolGroup.addTool(ZoomTool.toolName);
-    toolGroup.addTool(WindowLevelTool.toolName);
-
-    toolGroup.addViewport(viewportId, renderingEngineId);
-
-    // Set the windowLevel tool to be active when the mouse left button is pressed
-    toolGroup.setToolActive(WindowLevelTool.toolName, {
-      bindings: [
-        {
-          mouseButton: csToolsEnums.MouseBindings.Primary, // Left Click
-        },
-      ],
-    });
-
-    toolGroup.setToolActive(ZoomTool.toolName, {
-      bindings: [
-        {
-          mouseButton: csToolsEnums.MouseBindings.Secondary, // Right Click
-        },
-      ],
-    });
-
-
   }
-
-  //>>>>>>>>>>>>>>>>>
-
-
 
 }
 
