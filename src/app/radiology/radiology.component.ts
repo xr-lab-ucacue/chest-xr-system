f908f00e7eb8a4a67dd58300a11c31548e17907c
Pepe <95196085+PePeWee07@users.noreply.github.com>
2023-01-17 12:11:23 -0500

    funcional radiology

--- src/app/radiology/radiology.component.ts
+++ src/app/radiology/radiology.component.ts
@@ -5,6 +5,27 @@ import { ViewChild, ElementRef } from '@angular/core';
 //Alertas sweealert
 import Swal from 'sweetalert2'
 
+//Cornerstone
+import cornerstoneTools from 'cornerstone-tools';
+import * as cornerstone from 'cornerstone-core';
+import cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
+import * as dicomParser from 'dicom-parser';
+import cornerstoneMath from 'cornerstone-math';
+import 'hammerjs';
+
+//Config Cornestone
+var config = {
+  webWorkerPath:
+    '/assets/cornerstone/webworkers/cornerstoneWADOImageLoaderWebWorker.js',
+  taskConfiguration: {
+    decodeTask: {
+      codecsPath:
+        '/assets/cornerstone/codecs/cornerstoneWADOImageLoaderCodecs.js',
+    },
+  },
+};
+cornerstoneWADOImageLoader.webWorkerManager.initialize(config);
+
 @Component({
   selector: 'app-radiology',
   templateUrl: './radiology.component.html',
@@ -24,7 +45,7 @@ export class RadiologyComponent implements OnInit {
   diseases: any[] = [];
 
   // Options Horizontal Bar
-  view: [number, number] = [460, 660];
+  view: [number, number] = [550, 660];
   gradient: boolean = true;
   showXAxis = true;
   showYAxis = true;
@@ -148,8 +169,177 @@ export class RadiologyComponent implements OnInit {
         })
   }
 
+  activateTools(toolActive: string) {
+    const element = document.getElementById('element');
+
+    const LengthTool = cornerstoneTools.LengthTool;
+    const EllipticalRoiTool = cornerstoneTools.EllipticalRoiTool;
+    const ArrowAnnotateTool = cornerstoneTools.ArrowAnnotateTool;
+    const RotateTool = cornerstoneTools.RotateTool;
+    const WwwcTool = cornerstoneTools.WwwcTool; // brillo
+    const PanTool = cornerstoneTools.PanTool; // mover img por el canvas
+    const AngleTool = cornerstoneTools.AngleTool;
+    const BidirectionalTool = cornerstoneTools.BidirectionalTool; // crea una cruz tipo lenghtTool
+    const FreehandRoiTool = cornerstoneTools.FreehandRoiTool; // crea lineas a partir de otras (no para hatsa llegar al punto de inico)
+    const RectangleRoiTool = cornerstoneTools.RectangleRoiTool; // rectangulo calcula el area
+
+    cornerstone.enable(element);
+
+    switch (toolActive) {
+      case 'Length':
+        cornerstoneTools.addTool(LengthTool);
+        cornerstoneTools.setToolActive('Length', { mouseButtonMask: 1 });
+        break;
+
+      case 'EllipticalRoi':
+        cornerstoneTools.addTool(EllipticalRoiTool);
+        cornerstoneTools.setToolActive('EllipticalRoi', { mouseButtonMask: 1 });
+        break;
+
+      case 'ArrowAnnotate':
+        cornerstoneTools.addTool(ArrowAnnotateTool);
+        cornerstoneTools.setToolActive('ArrowAnnotate', { mouseButtonMask: 1 });
+        break;
+
+      case 'FreehandRoi':
+        cornerstoneTools.addTool(FreehandRoiTool);
+        cornerstoneTools.setToolActive('FreehandRoi', { mouseButtonMask: 1 });
+        break;
+
+      case 'Rotate':
+        cornerstoneTools.addTool(RotateTool);
+        cornerstoneTools.setToolActive('Rotate', { mouseButtonMask: 1 });
+        break;
+
+      case 'Wwwc':
+        cornerstoneTools.addTool(WwwcTool);
+        cornerstoneTools.setToolActive('Wwwc', { mouseButtonMask: 1 });
+        break;
+
+      case 'Pan':
+        cornerstoneTools.addTool(PanTool);
+        cornerstoneTools.setToolActive('Pan', { mouseButtonMask: 1 });
+        break;
+
+      case 'Angle':
+        cornerstoneTools.addTool(AngleTool);
+        cornerstoneTools.setToolActive('Angle', { mouseButtonMask: 1 });
+        break;
+
+      case 'Bidirectional':
+        cornerstoneTools.addTool(BidirectionalTool);
+        cornerstoneTools.setToolActive('Bidirectional', { mouseButtonMask: 1 });
+        break;
+
+      case 'RectangleRoi':
+        cornerstoneTools.addTool(RectangleRoiTool);
+        cornerstoneTools.setToolActive('RectangleRoi', { mouseButtonMask: 1 });
+        break;
+
+      case 'FreehandRoi':
+        cornerstoneTools.addTool(FreehandRoiTool );
+        cornerstoneTools.setToolActive('FreehandRoi', { mouseButtonMask: 1 });
+        break;
+
+      default:
+        console.log('No such day exists!');
+        break;
+    }
+  }
+
+  Tools() {
+    // Style de tools
+    const fontFamily =
+      'Work Sans, Roboto, OpenSans, HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif';
+    cornerstoneTools.textStyle.setFont(`16px ${fontFamily}`);
+    // Set the tool width
+    cornerstoneTools.toolStyle.setToolWidth(1);
+    // Set color for inactive tools
+    cornerstoneTools.toolColors.setToolColor('rgb(255, 255, 0)');
+    // Set color for active tools
+    cornerstoneTools.toolColors.setActiveColor('rgb(0, 255, 0)');
+
+    // Despligue de herramientas
+    const element = document.getElementById('element');
+
+    const ZoomMouseWheelTool = cornerstoneTools.ZoomMouseWheelTool; // zoom
+    const EraserTool = cornerstoneTools.EraserTool; // borrador
+    const MagnifyTool = cornerstoneTools.MagnifyTool; // lupa
+    const ScaleOverlayTool = cornerstoneTools.ScaleOverlayTool; // escala
+    const OrientationMarkersTool = cornerstoneTools.OrientationMarkersTool; // letras de orientacion
+
+    //toll activa
+    const LengthTool = cornerstoneTools.LengthTool;
+
+    cornerstone.enable(element);
+
+    //primera toll activa
+    cornerstoneTools.addTool(LengthTool);
+    cornerstoneTools.setToolActive('Length', { mouseButtonMask: 1 });
+
+    cornerstoneTools.addTool(ZoomMouseWheelTool);
+    cornerstoneTools.addTool(EraserTool);
+    cornerstoneTools.addTool(MagnifyTool);
+    cornerstoneTools.addTool(ScaleOverlayTool);
+    cornerstoneTools.addTool(OrientationMarkersTool);
+
+
+    //herramientas activas por defecto
+    cornerstoneTools.setToolActive('ZoomMouseWheel', { mouseButtonMask: 0 }); // rueda de maus
+    cornerstoneTools.setToolActive('Magnify', { mouseButtonMask: 4 }); // boton rueda
+    cornerstoneTools.setToolActive('Eraser', { mouseButtonMask: 2 }); //  click derecho
+    cornerstoneTools.setToolActive('ScaleOverlay', { mouseButtonMask: 0 });
+    cornerstoneTools.setToolActive('OrientationMarkers', {
+      mouseButtonMask: 0,
+    });
+  }
+
+  initCornerstone() {
+    cornerstoneTools.external.cornerstone = cornerstone;
+    cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
+    cornerstoneTools.external.Hammer = Hammer;
+    cornerstoneTools.init();
+
+    cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
+    cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
+
+    var element = document.getElementById('element');
+
+    cornerstone.enable(element);
+    this.Tools();
+
+    cornerstone
+      .loadAndCacheImage('wadouri:' + '../../assets/IM222')
+      .then((imageData) => {
+        console.log(imageData);
+        cornerstone.displayImage(element, imageData);
+
+        //   var viewport = {
+        //   invert: false,
+        //   pixelReplication: false,
+        //   voi: {
+        //     windowWidth: 400,
+        //     windowCenter: 60,
+        //   },
+        //   scale: 1.1,
+        //   translation: {
+        //     x: 0,
+        //     y: 0,
+        //   },
+        //   // colormap: 'hot',
+        // };
+
+        // cornerstone.setViewport(element, viewport);
+        // cornerstone.updateImage(element);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   ngOnInit(): void {
     this.myColor();
     this.diaseasesOnly();
+    this.initCornerstone();
   }
 }
